c527ee824186f719d0bd9192d369a84b32c34b36
Alexander Granhof <alexander.g737@gmail.com>
2020-05-10 21:55:18 +0200

    Created a simple game component

---
+++ client/src/components/grid/Grid.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Grid.scss";
+
+const Grid = () => {
+    return (
+        <div className="grid">
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+            <div className="square"></div>
+        </div>
+    )
+};
+
+export default Grid;
