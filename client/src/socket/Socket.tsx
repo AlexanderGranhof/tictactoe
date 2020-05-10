b5749bf3d32f60b4801bb4c3318a1d4ec4814ac9
Alexander Granhof <alexander.g737@gmail.com>
2020-05-10 21:55:37 +0200

    changed so you have to manually connect

--- client/src/socket/Socket.tsx
+++ client/src/socket/Socket.tsx
@@ -5,9 +5,13 @@ class Socket {
 
     constructor() {
         console.warn("CREATING NEW CONNECTION");
-        this.socket = io("http://localhost:8080");
+        this.socket = io("http://localhost:8080", { autoConnect: false });
 
         this.socket.on("connect", () => console.log("[socket] connected"));
+    }
+
+    connect() {
+        this.socket.connect();
     }
 }
 
