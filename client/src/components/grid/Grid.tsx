da8a682743cd034dcdc39acd0cb7ed472d92e87c
Alexander Granhof <alexander.g737@gmail.com>
2020-05-10 22:47:13 +0200

    Fixed types and proper rendering of the squares

--- client/src/components/grid/Grid.tsx
+++ client/src/components/grid/Grid.tsx
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
 import "./Grid.scss";
+import Game, { Board, Player, Opponent, Empty } from "../../game/Game";
+
+const getSquareIcon = (position: -1 | 0 | 1) => {
+    switch(position) {
+        case 0: {
+            return "o"
+        }
+
+        case 1: {
+            return "x"
+        }
+
+        default: {
+            return ""
+        }
+    } 
+}
+
+const a: Array<Player | Opponent | Empty> = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
 
 const Grid = () => {
+    const [ game, setGame ]: [Game | undefined, Dispatch<SetStateAction<Game | undefined>>] = useState();
+    const [ grid, setGrid ]: [ Array<Empty | Player | Opponent>, Dispatch<SetStateAction<Array<Empty | Player | Opponent>>> ] = useState([-1, -1, -1, -1, -1, -1, -1, -1, -1] as Array<Empty | Player | Opponent>);
+
+    useEffect(() => {
+        const game = new Game();
+        game.setStartingPlayer(Game.PLAYER);
+
+        setGame(game);
+    }, []);
+
+    const handleSquareClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (!game) return;
+
+        const { currentTarget } = event;
+
+        if (game.currentTurn !== Game.PLAYER) {
+            return console.warn("not your turn yet")
+        }
+
+        const position = parseInt(currentTarget.dataset.position || "");
+
+        if (isNaN(position)) {
+            throw new Error("attribute 'data-position' was not found on clicked element");
+        }
+
+        game.setMove(position);
+        setGrid(game.board.positions);
+    };
+
+
     return (
         <div className="grid">
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
-            <div className="square"></div>
+            {
+                grid.map((postion, index) => (
+                    <div data-position={index} onClick={handleSquareClick} key={index} className="square">{getSquareIcon(postion)}</div>
+                ))
+                
+            }
         </div>
     )
 };
