8ccc3987b4cfa8533c5583298781d9a4eede3586
Alexander Granhof <alexander.g737@gmail.com>
2020-06-03 23:08:15 +0200

    created a class that handles lobbies

---
+++ server/src/models/lobbyHandler.ts
@@ -0,0 +1,66 @@
+interface LobbyConstructor {
+    id: string,
+    createdBy: string,
+}
+
+class Lobby {
+    id: string;
+    createdAt: Date;
+    createdBy: string;
+
+    constructor({id, createdBy}: LobbyConstructor) {
+        this.id = id;
+        this.createdAt = new Date();
+        this.createdBy = createdBy;
+    }
+}
+
+class LobbyHandler {
+    private lobbies: { [key: string]: Lobby }
+
+    constructor() {
+        this.lobbies = {};
+    }
+
+    static generateLobbyID(length: number = 4) {
+        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+        const numbers = "0123456789";
+    
+        let roomID = "";
+    
+        for (let i = 0; i < length; i++) {
+            const useLetter = Math.random() > 0.5;
+    
+            const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+            const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+    
+            roomID += useLetter ? randomLetter : randomNumber;
+        }
+    
+        return roomID;
+    }
+
+    lobbyExists(id: string) {
+        return Object.keys(this.lobbies).includes(id);
+    }
+
+    createLobby(createdBySocketID: string) {
+        let id = LobbyHandler.generateLobbyID();
+
+        while (this.lobbyExists(id)) {
+            id = LobbyHandler.generateLobbyID();
+        }
+
+        this.lobbies[id] = new Lobby({ id, createdBy: createdBySocketID })
+    }
+
+    removeLobby(id: string) {
+        const lobbyExists = this.lobbyExists(id);
+
+        if (lobbyExists) {
+            delete this.lobbies[id]
+        }
+
+        return lobbyExists
+    }
+}
