442c4ae2bc0fec4b2ee890c2ceb02e4e5b34b962
Alexander Granhof <alexander.g737@gmail.com>
2020-06-04 21:53:44 +0200

    added new handlers to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
+require("source-map-support").install();
+
 import SocketIO from "socket.io";
 import http from "http";
+
+import GameHandler from "./handlers/game";
+import LobbyHandler from "./handlers/lobby";
 
 const server = http.createServer();
 const options = {};
 
 const io = SocketIO(server, options);
 
-io.on("connection", console.log);
+io.on("connection", socket => {
+    console.log("Got connection", socket.id)
+    GameHandler(socket);
+    LobbyHandler(socket);
+});
 
 server.listen(8080);
