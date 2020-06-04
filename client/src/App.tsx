dd4ccaf0946354dd486910d5e465f408e7a9eec9
Alexander Granhof <alexander.g737@gmail.com>
2020-06-04 21:51:05 +0200

    Added transition groups so that rotues fade in and out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { UserProvider } from "./context/user";
 
 import Grid from "./components/grid/Grid";
+
 import Lobby from "./views/Lobby/Lobby";
+import Home from "./views/Home/Home";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Lobby} />
-            </Switch>
-        </BrowserRouter>
+        <UserProvider>
+            <BrowserRouter>
+            <nav style={{zIndex: 100, position: "absolute", right: 0}}>
+                <Link to="/">home</Link>
+                <Link to="/lobbies">lobbies</Link>
+            </nav>
+                <Route render={({ location }) => (
+                    <TransitionGroup>
+                        <CSSTransition
+                            key={location.key}
+                            timeout={300}
+                            classNames="page-fade"
+                        >
+                            <Switch location={location}>
+                                <Route exact path="/" component={Home} />
+                                <Route exact path="/lobbies" component={Lobby} />
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
+                )} />
+            </BrowserRouter>
+        </UserProvider>
+
     );
 }
 
