8af4661929ff828bf9c0c7cba036d02bc5bffdd7
Alexander Granhof <alexander.g737@gmail.com>
2020-06-04 21:50:19 +0200

    Added table to the lobby view

--- client/src/views/Lobby/Lobby.tsx
+++ client/src/views/Lobby/Lobby.tsx
@@ -1,14 +1,31 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useContext, useState } from "react";
 import socket from "../../models/socket";
+import Table from "../../components/Table/Table";
+import { userContext } from "../../context/user";
 
 const Lobby: FunctionComponent = (props) => {
+    const [userState, setUserState] = useContext(userContext);
 
-    const handleLobbyCreate = () => {
-        socket.emit("create_lobby", "ayy lmao")
+    useEffect(() => {
+        socket.emit("get_clients", (clients: any) => {
+            console.log("we got", clients)
+        });
+
+        socket.on("room_created", (room: any) => {
+            console.log("we got a room", room)
+        })
+    }, []);
+
+    const handleRoomCreate = () => {
+        socket.emit("create_room", useState)
     };
 
     return (
-        <button onClick={handleLobbyCreate}>create lobby</button>
+        <main className="page">
+            <h1>Welcome {userState.name}</h1>
+            <button onClick={handleRoomCreate}>create room</button>
+            <Table />
+        </main>
     )
 };
 
