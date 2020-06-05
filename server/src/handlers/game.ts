d0df8f7a3830c14128308520774d696337df7ba2
Alexander Granhof <alexander.g737@gmail.com>
2020-06-05 13:23:12 +0200

    Added server parameter to be able to send global messages

--- server/src/handlers/game.ts
+++ server/src/handlers/game.ts
@@ -1,6 +1,6 @@
-import { Socket } from "socket.io";
+import { Socket, Server } from "socket.io";
 
-export default (socket: Socket) => {
+export default (socket: Socket, server: Server) => {
     console.log("sending room")
     socket.emit("room", "abc")
 };
--- server/src/handlers/lobby.ts
+++ server/src/handlers/lobby.ts
@@ -1,4 +1,4 @@
-import { Socket } from "socket.io";
+import { Socket, Server } from "socket.io";
 import LobbyHandler from "../models/lobbyHandler";
 import { createLogger } from "../helpers/logger";
 import chalk from "chalk";
@@ -7,7 +7,7 @@ const log = createLogger("[HANDLERS:LOBBY]")
 
 const handler = new LobbyHandler();
 
-export default (socket: Socket) => {
+export default (socket: Socket, server: Server) => {
     log("attaching lobby stuff")
     socket.join("lobby");
 
@@ -15,7 +15,7 @@ export default (socket: Socket) => {
         const room = handler.createLobby({ hostName: data.name, hostSocketId: socket.id });
         log("got room", room)
 
-        socket.broadcast.to("lobby").emit("room_created", room)
+        server.in("lobby").emit("room_created", room)
 
         socket.emit("created_room", room);
     });
