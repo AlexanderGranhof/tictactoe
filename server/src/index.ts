baf5842d72484a58f2bb3a8053fd184a833e1d29
Alexander Granhof <alexander.g737@gmail.com>
2020-06-05 13:23:27 +0200

    Added new server parameter

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,15 +6,15 @@ import http from "http";
 import GameHandler from "./handlers/game";
 import LobbyHandler from "./handlers/lobby";
 
-const server = http.createServer();
+const httpServer = http.createServer();
 const options = {};
 
-const io = SocketIO(server, options);
+const server = SocketIO(httpServer, options);
 
-io.on("connection", socket => {
+server.on("connection", socket => {
     console.log("Got connection", socket.id)
-    GameHandler(socket);
-    LobbyHandler(socket);
+    GameHandler(socket, server);
+    LobbyHandler(socket, server);
 });
 
 server.listen(8080);
