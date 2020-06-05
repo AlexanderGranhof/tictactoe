bab1054ab1d85ab30abf38f08ecaf92ce5f1142e
Alexander Granhof <alexander.g737@gmail.com>
2020-06-05 17:38:52 +0200

    Home now checks for basic profanity usernames

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React, { FunctionComponent, useRef, RefObject, MutableRefObject, useState, useEffect, useContext } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React, { FunctionComponent, useRef, useContext, useEffect, useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import "./Home.scss";
 import { userContext } from "../../context/user";
 import { RouteComponentProps } from "react-router-dom";
@@ -8,36 +8,77 @@ const Home: FunctionComponent<RouteComponentProps> = props => {
     const inputRef = useRef<HTMLInputElement>(null);
     const [ userState, setUserState ] = useContext(userContext);
     const { history } = props;
+    const [isBadName, setIsBadName] = useState(false);
+
+    const [badwords, setBadwords] = useState<string[]>([]);
 
     const handlePageClick = () => {
         inputRef.current && inputRef.current.focus();
     };
 
+    const fetchBadWords = async () => {
+        const response = await fetch("/badwords.txt");
+        const data = await response.text();
+
+        setBadwords(data.split("\n"));
+    }
+
+    const isBadWord = (word: string) => {
+        return badwords.some(badword => {
+            word = word.toLowerCase()
+            badword = badword.toLowerCase()
+
+            const result = word === badword && word.length;
+
+            result && console.warn("bad name", word, badword)
+
+            return result;
+        })
+    }
+
     const handleKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
         const isEnter = event.key === "Enter";
+        const name = event.currentTarget.value;
+        const isBad = isBadWord(name);
 
-        if (isEnter) {
-            const name = event.currentTarget.value;
+        if (inputRef.current) {
+            inputRef.current.style.width = `${(name.length + 2) * 28}px`
+        }
+        
+
+        if (isEnter && !isBad) {
             setUserState({...userState, name})
-
             history.push("/lobbies")
         }
+
+        setIsBadName(isBad);
     }
+
+    useEffect(() => {
+        fetchBadWords()
+    }, [])
 
     return (
         <main className="page welcome-fade">
             <div className="welcome-box" onClick={handlePageClick}>
                 <div className="welcome-inputs">
-                        <CSSTransition
-                            // key={1}
-                            timeout={300}
-                            in={true}
-                            appear={true}
-                            classNames="myFade"
-                        >
-                            <h1 className="welcome-title">Welcome, please enter your name</h1>
-                        </CSSTransition>
-                    <input onKeyDown={handleKeydown} ref={inputRef} autoFocus spellCheck={false} className="welcome-username" type="text"/>
+                    <CSSTransition
+                        // key={1}
+                        timeout={300}
+                        in={true}
+                        appear={true}
+                        classNames="myFade"
+                    >
+                        <h1 className="welcome-title">Welcome, please enter your name</h1>
+                    </CSSTransition>
+                    <CSSTransition
+                        in={isBadName}
+                        timeout={300}
+                        classNames="bad-word"
+                    >
+                        <h4 className="error-title">please choose a different name</h4>
+                    </CSSTransition>
+                    <input onKeyUp={handleKeydown} ref={inputRef} autoFocus spellCheck={false} className={`welcome-username ${isBadName ? "bad-word" : ""}`} type="text"/>
                 </div>
             </div>
         </main>
