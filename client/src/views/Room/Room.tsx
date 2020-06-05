9d6f5504999ae8efcea5705b0992026397050edc
Alexander Granhof <alexander.g737@gmail.com>
2020-06-05 17:37:17 +0200

    Created an initial room component

---
+++ client/src/views/Room/Room.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Room: FunctionComponent = props => {
+    return (
+        <main className="page">
+            <h1>Room</h1>
+        </main>
+    )
+};
+
+export default Room;
