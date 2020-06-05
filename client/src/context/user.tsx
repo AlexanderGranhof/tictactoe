5129ef8fe4f4462f68c461a9d5a2e6a810f03d40
Alexander Granhof <alexander.g737@gmail.com>
2020-06-05 17:39:19 +0200

    User context now loads and saves in localstore as cache

--- client/src/context/user.tsx
+++ client/src/context/user.tsx
@@ -1,13 +1,27 @@
 import React, { createContext, FunctionComponent, useState } from "react";
 
-const initialContext = {
-    name: ""
-};
+const baseContext = { name: "" }
+
+let initialContext: any;
+
+const cachedString = localStorage.getItem("context.userContext");
+
+try {
+    initialContext = cachedString === null ? baseContext : JSON.parse(cachedString);
+} catch {
+    initialContext = baseContext
+}
 
 const userContext = createContext<any>([]);
 
 const UserProvider: FunctionComponent = ({children}) => {
-    const [ userState, setUserState ] = useState(initialContext);
+    const [ userState, setState ] = useState(initialContext);
+
+    const setUserState = (data: any) => { 
+        localStorage.setItem("context.userContext", JSON.stringify(data))
+        
+        return setState(data);
+    }
 
     return (
         <userContext.Provider value={[userState, setUserState]}>
