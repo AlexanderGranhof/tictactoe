f4b2c04a9f18041084cdd26e97cce663f8243be4
Alexander Granhof <alexander.g737@gmail.com>
2020-06-06 15:37:32 +0200

    check to see if lobby is valid, otherwise go back to lobbies

--- client/src/views/Room/Room.tsx
+++ client/src/views/Room/Room.tsx
@@ -1,6 +1,25 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect } from "react";
+import socket from "../../models/socket";
+import { RouteComponentProps } from "react-router-dom";
 
-const Room: FunctionComponent = props => {
+const Room: FunctionComponent<RouteComponentProps> = props => {
+    const { match, history } = props;
+    const { id } = match.params as any;
+
+    const onViewLeave = () => {
+        socket.emit("game_leave")
+    };
+
+    useEffect(() => {
+        socket.emit("game_join", id, (validRoom: boolean) => {
+            if (!validRoom) {
+                history.push("/lobbies")
+            }
+        })
+
+        return onViewLeave;
+    }, []);
+
     return (
         <main className="page">
             <h1>Room</h1>
