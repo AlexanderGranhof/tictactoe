2528cbfa78713e75b278b8f5d90da11be4bd40fb
Alexander Granhof <alexander.g737@gmail.com>
2020-06-06 15:37:46 +0200

    add listeners when clients join games

--- server/src/handlers/game.ts
+++ server/src/handlers/game.ts
@@ -1,9 +1,40 @@
 import { Socket, Server } from "socket.io";
 import { createLogger } from "../helpers/logger";
+import handler from "../models/lobbyHandler";
+import * as filter from "../helpers/filter";
 
-const log = createLogger("[handlers:game.ts]")
+
+const log = createLogger("[handlers:game.ts]");
+
+const leaveAllRooms = (socket: Socket, server: Server) => {
+    const rooms = filter.gameRooms(socket);
+
+    rooms.forEach(room => {
+        handler.removeClient(socket, room);
+
+        if (handler.roomIsEmpty(room)) {
+            handler.removeRoom(room);
+            server.in("lobby").emit("room_removed", room);
+        }
+
+        socket.broadcast.to(room).emit("partner_leave", socket.id)
+        socket.leave(room);
+    })
+}
 
 export default (socket: Socket, server: Server) => {
-    log("Attaching game listeners")
-    socket.emit("room", "abc")
+    socket.on("game_join", (room: any, cb: Function) => {
+        const roomExists = handler.roomExists(room);
+
+        if (roomExists) {
+            handler.addClient(socket, room);
+            socket.broadcast.to(room).emit("partner_join", socket.id)
+            socket.join(room);   
+        }
+
+        cb(roomExists)
+    });
+
+    socket.on("game_leave", () => leaveAllRooms(socket, server))
+    socket.on("disconnecting", () => leaveAllRooms(socket, server));
 };
