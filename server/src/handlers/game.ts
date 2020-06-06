0c84547ef06daa43366bcf81b2fd06988436eab8
Alexander Granhof <alexander.g737@gmail.com>
2020-06-06 17:50:22 +0200

    added move event that will update the room game state

--- server/src/handlers/game.ts
+++ server/src/handlers/game.ts
@@ -26,14 +26,40 @@ export default (socket: Socket, server: Server) => {
     socket.on("game_join", (room: any, cb: Function) => {
         const roomExists = handler.roomExists(room);
 
+        let state: any = null;
+
         if (roomExists) {
             handler.addClient(socket, room);
-            socket.broadcast.to(room).emit("partner_join", socket.id)
-            socket.join(room);   
+            state = handler.getRoomState(room);
+            socket.broadcast.to(room).emit("opponent_join", state)
+            
+            return socket.join(room, () => cb({ valid: roomExists, state }));
         }
 
-        cb(roomExists)
+        cb({ valid: roomExists, state })
     });
+
+    socket.on("move", index => {
+        const room = filter.gameRooms(socket).pop();
+
+        if (!room) {
+            throw new Error(`unable to find room of socket ${socket.id}, ${JSON.stringify(socket.rooms)}`)
+        }
+
+        const state = handler.getRoomState(room);
+
+        if (state.secondMovePlayer === "") {
+            return socket.emit("move_error", "you must wait until an opponent joins")
+        }
+
+        try {
+            state.makeMove(index, socket.id);
+    
+            server.in(room).emit("game_state", state)
+        } catch (error) {
+            socket.emit("move_error", error.message)
+        }
+    })
 
     socket.on("game_leave", () => leaveAllRooms(socket, server))
     socket.on("disconnecting", () => leaveAllRooms(socket, server));
