ad310b5f18d12104619d849df46b672bb177a6ed
Alexander Granhof <alexander.g737@gmail.com>
2020-06-13 00:20:48 +0200

    added some alerts when connecting and disconnecting to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
+import 'antd/dist/antd.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route, Link, Redirect } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { UserProvider } from "./context/user";
+import socket from "./models/socket";
+import { message } from "antd";
+
 
 import Lobby from "./views/Lobby/Lobby";
 import Home from "./views/Home/Home";
 import Room from "./views/Room/Room";
+
+socket.on("connect", () => {
+    message.success(("connected to socket server!"))
+    
+    socket.once("connect_error", () => message.error("disconnected from socket server!"))
+});
+
+socket.on("reconnect", () => {
+    message.success(("reconnected to socket server!"))
+
+    socket.once("connect_error", () => message.error("disconnected from socket server!"))
+})
 
 function App() {
     return (
